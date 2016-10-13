636dd3261486e58bb4a36cb8d7bfd6321c0a7442
Rob Rivera <rjrivera@mit.edu>
2016-10-13 16:42:15 -0400

    modified copyright statement

--- tetrisClient/client.cpp
+++ tetrisClient/client.cpp
@@ -1,11 +1,14 @@
 //
 //// client.cpp
 //// ~~~~~~~~~~
-////
+//// lines 25 - 60 
 //// Copyright (c) 2003-2016 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 ////
 //// Distributed under the Boost Software License, Version 1.0. (See accompanying
 //// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//	remaining code
+//// Copyright (c) 2016 Robert J Rivera same license as above. 
+//	
 ////
 
 #include <iostream>
