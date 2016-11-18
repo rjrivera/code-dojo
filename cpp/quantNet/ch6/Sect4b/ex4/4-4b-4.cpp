48f42fbee823926bab0bde88839d8c2c2a490156
Rob Rivera <rjrivera@mit.edu>
2016-11-18 11:51:05 -0500

    ex4 implemented - 35 minutes?

---
+++ cpp/quantNet/ch6/Sect4b/ex4/4-4b-4.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+#include "Point.hpp"
+#include "Line.hpp"
+#include "Array.cpp"
+#include "Circle.hpp"
+#include "Shape.hpp"
+#include "OutOfBoundsException.hpp"
+#include "NumericArray.cpp"
+#include "PointArray.cpp"
+#include "Stack.cpp"
+
+using namespace std;
+
+/*
+	RELEASE NOTE v 4-4b-1
+
+	Implemented a Templated Stack class and used the Array<T> class as a data member for the data storage component demonstrating the composition concept of OOP.
+
+	Current file implements tests to validate proper implementation of the Stack API annotated in Stack.hpp
+
+*/
+
+int main() {
+	Stack<int> myS(10);
+	myS.push(5);
+	myS.push(4);
+	myS.pop();
+	myS.push(2);
+	// should yield 2;
+	cout << myS.front() << endl;
+	// should yield 5;
+	myS.pop();
+	cout << myS.front() << endl;
+	myS.push(10);
+	
+	cout << myS.front() << endl;
+	myS.pop();
+	myS.pop();
+
+
+	return 0 ;
+}
---
+++ cpp/quantNet/ch6/Sect4b/ex4/NumericArray.cpp
@@ -0,0 +1,181 @@
+#ifndef NUMERICARRAY_CPP
+#define NUMERICARRAY_CPP
+
+#include "NumericArray.hpp"
+//#include "Array.hpp"
+#include <iostream>
+#include <string>
+
+/*
+private:
+	int m_length;
+	T * m_data;
+
+	RELEASE NOTES v4-4b
+
+	Templatized NumericArray class
+
+*/
+
+
+//template<class T>
+//int NumericArray<T>::def_size = 10;
+
+template<class T> 
+NumericArray<T>::NumericArray() : Array<T>() {
+	
+}
+
+/*
+constructor with size
+*/
+template<class T>
+NumericArray<T>::NumericArray(int size_) : Array<T>(size_) {
+}	
+
+/*
+copy constructor
+*/
+template<class T>
+NumericArray<T>::NumericArray(const NumericArray<T>& source_) {
+	//create the size of memory needed. 
+	Array<T>::m_length = source_.Size();
+
+	Array<T>::m_data = new T[Array<T>::m_length];
+	//begin transcribing from source to target. 
+	
+	for (int i = 0; i < Array<T>::m_length; i++) {
+		Array<T>::m_data[i] = source_.Array<T>::m_data[i]; 
+	}
+}
+
+template<class T>
+NumericArray<T>::~NumericArray() {
+	
+	//delete[] Array<T>::m_data;
+}
+
+/*
+Accessor - get by index. same functionality as operator[]
+*/
+template<class T>
+T NumericArray<T>::GetElement(int index_) const {
+	if (index_ >= 0 && index_ < Array<T>::m_length) { 
+		return Array<T>::m_data[index_];
+	}
+	else { throw OutOfBoundsException(index_);}
+}
+
+template<class T>
+void NumericArray<T>::DefaultSize(int index_) {
+	Array<T>::def_size = index_;
+}
+
+template<class T>
+int NumericArray<T>::DefaultSize() const {
+	return Array<T>::def_size;
+}
+
+template<class T>
+NumericArray<T>& NumericArray<T>::operator* (int index_) {
+	for (int i = 0; i < Array<T>::m_length; i++) {
+		Array<T>::m_data[i] *= index_;
+	}
+	return *this;
+}
+
+template<class T>
+NumericArray<T>& NumericArray<T>::operator+(NumericArray<T>& index_) {
+	if (Array<T>::m_length != index_.Array<T>::m_length )  {
+		std::cout << "two candidate arrays for scaling not equal\n";
+		return *this;
+	}
+	for (int i = 0; i < Array<T>::m_length; i++) {
+		Array<T>::m_data[i] += index_.Array<T>::m_data[i];
+	}
+	return *this;
+}
+
+template<class T>
+T NumericArray<T>::DotProduct(NumericArray<T>& b_) {
+	if (Array<T>::m_length != b_.Array<T>::m_length )  {
+		std::cout << "two candidate arrays for DP not equal\n";
+		return 0;
+	}
+	T dP = 0;
+	for (int i = 0; i < Array<T>::m_length; i++) {
+		dP += b_.Array<T>::m_data[i] * Array<T>::m_data[i];
+	}
+	return dP;
+
+}
+
+
+/*
+ *
+ *
+assignment operator
+*/
+/*
+template<class T>
+Array<T>& Array<T>::operator=(const Array<T>& source_) {
+	m_length = source_.Size();
+
+	if (&source_ == this) {return *this;} //handle self-assignment. 
+	//INITIALIZE ARRAY OF OBJECTS. 
+	delete[] m_data;
+	m_data = new T[m_length];
+	
+	for (int i = 0; i < m_length; i++) {
+		m_data[i] = source_.m_data[i]; //amended line. 
+	}	
+
+	return *this;
+}
+template<class T>
+T& Array<T>::operator[](int index_) {
+	if (index_ >= 0 && index_ <  m_length) {
+		return m_data[index_];
+	}
+	else { throw OutOfBoundsException(index_);}
+}	
+
+template<class T>
+const T& Array<T>::operator[](int index_) const {
+	if (index_ >= 0 && index_ < m_length) { 
+		return m_data[index_];
+	} 
+	else {	throw OutOfBoundsException(index_);}
+	
+}
+template<class T>
+Array<T>::~Array() {
+	
+	delete[] m_data; 
+}
+
+*/
+/*
+template<class T>
+int Array<T>::Size() const {
+	return m_length;
+}
+
+template<class T>
+void Array<T>::SetElement(int index_, T& target_) {
+	if (index_ >= 0 || index_ < m_length) { 
+		m_data[index_] = target_;
+	}
+	else {throw OutOfBoundsException(index_);}
+
+}
+*/
+//===========================
+/*
+template class Array<Point>;
+template class Array<Circle>;
+template class Array<Line>;
+template class Array<int>;
+*/
+
+#endif
