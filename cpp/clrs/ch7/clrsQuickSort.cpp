38ae3740398de8ee97ef078b440bc2339e1be936
Rob Rivera <rjrivera@mit.edu>
2017-02-07 12:18:33 -0500

    finished my implementation from memory, now reading the actual chapter

--- cpp/clrs/ch7/clrsQuickSort.cpp
+++ cpp/clrs/ch7/clrsQuickSort.cpp
@@ -1,25 +1,82 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
 
 using namespace std;
 
+
+template<class T>
+void swap(vector<T>& input_, int lV, int rV) {
+	T temp = input_[lV];
+	input_[lV] = input_[rV];
+	input_[rV] = temp;
+}
 
 /* Using templating to get my ass used to using them. 
  *
  */
 template<class T>
-void quickSort(vector<T> input_) {
+void qSortHelper(vector<T>& input_, int lB, int rB) {
+	if (rB-lB <= 1) return;
+	//calculate the middle value
+	int midVal =  ((rB-lB)/2) + lB;
 
-	for(T& val : input_) cout << val << endl;
+	//pick a pivot value - in this implementation, it will be the mid value 
+	T pV = input_[midVal];
+	int pVInd = lB;
+	//swap the pV inplace with the lB value
+	swap<T>(input_, lB, midVal);
+	//begin comparing each value with the pviot value. 
+	for (int i = lB+1; i <= rB; i++) {
+		if (input_[i] < pV) {
+			//swap the value in question with the pv
+			swap<T>(input_, pVInd, i);
+			pVInd++;
+			//inc the pvInd then put it back in it's place. 
+			swap<T>(input_, pVInd, i);
+		}
+		//else do nothing.
+
+	}
+	//divide and conquer based on the pvIndex after iterating
+	//through the current partition. 
+	qSortHelper<T>(input_, lB, pVInd-1);
+	qSortHelper<T>(input_, pVInd+1, rB);
+	
+
+}
+
+template<class T>
+void quickSort(vector<T>& input_) {
+
+	qSortHelper<T>(input_, 0, input_.size()-1);
 };
 
 int main( int argc, char** argv ) {
 	vector<int> batchOne;
-	for (int i = 0; i < 10; i++) {
-		batchOne.push_back(i);
-	}
+	batchOne.push_back(5);
+	batchOne.push_back(3);
+	batchOne.push_back(10);
+	batchOne.push_back(45);
+	batchOne.push_back(33);
+	batchOne.push_back(8);
+	
 
 	quickSort<int>(batchOne);
+	for (int& val : batchOne) cout << val << endl;
 
+	vector<string> batchTwo;
+	batchTwo.push_back("greetings");
+	batchTwo.push_back("cerebrate");
+	batchTwo.push_back("i");
+	batchTwo.push_back("hope");
+	batchTwo.push_back("your");
+	batchTwo.push_back("slumber");
+	batchTwo.push_back("served");
+	batchTwo.push_back("well");
+
+	quickSort<string>(batchTwo);
+	for (string& val : batchTwo) cout << val << endl;
 	return 0;
 }
