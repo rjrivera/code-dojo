e5f208e6c8aee9ed4457f603ed4c44e422a59b17
Rob Rivera <rjrivera@mit.edu>
2018-10-14 13:55:38 -0400

    arith operations

---
+++ machL/cookbook/1-18addSubMats.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+vect_A  = np.array( [ 1, 2, 3 ] )
+vect_B  = np.array( [ 2, 2, 2 ] )
+
+
+
+print( "vect A: ", vect_A, "\nvect B:", vect_B,
+    "\nadd: ", np.add( vect_A, vect_B ),
+    "\nsub: ", np.subtract( vect_A, vect_B ) )
+
+
